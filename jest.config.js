391d2fed26dd871326487a5ffc70239d4d92ec78
josephmannis <annis.jo@husky.neu.edu>
2020-07-28 21:30:56 -0400

    Configure jest.:

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "jest-expo",
+    moduleFileExtensions: ['js','jsx','json', 'ts', 'tsx'],
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    },
+    testMatch: [
+      "**/*.test.ts?(x)"
+    ]
+  };
